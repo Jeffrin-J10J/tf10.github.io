42a08cd425c3e5550134c5e15c9eddba0f92364e
Jeffrin <52578715+Jeffrin-J10J@users.noreply.github.com>
2024-07-07 11:52:50 +0530

    finally-color-selected

--- Toolfe/assets/js/theme-worldmap.js
+++ Toolfe/assets/js/theme-worldmap.js
@@ -141,7 +141,7 @@ var map = new jsVectorMap({
   markerStyle: {
     initial: {
       r: 7,
-      fill: "#1e1e1e",
+      fill: "#FF4E00",
       fillOpacity: 1,
       stroke: "#FFF",
       strokeWidth: 5,
