160701f92f60479ab651bda27a94999c56e9760b
Dhanush16dd <dhanush11ynt@gmail.com>
2024-07-16 11:18:04 +0530

    files changed into php

--- Toolfe/trust-center/cookie-policy.php
+++ Toolfe/trust-center/cookie-policy.php
@@ -1,44 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-
-
-<head>
-    <!-- ========== Meta Tags ========== -->
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <!-- ========== Page Title ========== -->
-    <title>Cookie Policy | Toolfe</title>
-
-  <!-- ========== Start Fonts Style ========== -->
-  <link rel="preconnect" href="https://fonts.googleapis.com/" />
-  <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin />
-  <link
-    href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100;0,9..40,200;0,9..40,300;0,9..40,400;0,9..40,500;0,9..40,600;0,9..40,700;0,9..40,800;1,9..40,100;1,9..40,200;1,9..40,300;1,9..40,400;1,9..40,500;1,9..40,600&amp;family=Syne:wght@400;500;600&amp;family=Yantramanav:wght@100;300;400;500;700;900&amp;display=swap"
-    rel="stylesheet"
-  />
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/iconoir-icons/iconoir@main/css/iconoir.css"/>
-
-  <!-- ========== Start Stylesheet ========== -->
-  <link
-    rel="stylesheet"
-    href="../../../cdn.jsdelivr.net/gh/iconoir-icons/iconoir%40main/css/iconoir.css"
-  />
-  <link
-    rel="stylesheet"
-    href="https://cdnjs.cloudflare.com/ajax/libs/line-awesome/1.3.0/line-awesome/css/line-awesome.min.css"
-  />
-  <link rel="stylesheet" href="../assets/css/bootstrap.min.css" />
-  <link rel="stylesheet" href="../assets/css/swiper-bundle.min.css" />
-  <link rel="stylesheet" href="../assets/css/style.css" />
-  <link rel="stylesheet" href="../assets/css/responsive.css" />
-    <!-- ========== End Stylesheet ========== -->
-
-
-</head>
-
-<body>
+<?php 
+include '../includes/header.php'
+?>
 
     <!-- Main -->
     <main class="main-page case-studie-details-page">
@@ -132,18 +94,6 @@
      
     </main>
 
-    <!-- jQuery Frameworks
-    ============================================= -->
-    <script src="../assets/js/jquery-3.7.0.min.js"></script>
-    <script src="../assets/js/bootstrap.bundle.min.js"></script>
-    <script src="../assets/js/gsap.min.js"></script>
-    <script src="../assets/js/Draggable.min.js"></script>
-    <script src="../assets/js/swiper-bundle.min.js"></script>
-    <script src="../assets/js/client-marquee.js"></script>
-    <script src="../assets/js/theme-custom.js"></script>
-    <script src="../assets/js/form1.js"></script>
-    <script src="../assets/js/subscribe-form.js"></script>
-</body>
-
-
-</html>
+<?php 
+include '../includes/footer.php'
+?>
